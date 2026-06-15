55b15100f0e0d864e495c8a48859b03ebf7483e8
Dead Mouse <163331702+NanoMindExplorer@users.noreply.github.com>
2026-06-16 00:06:44 +0700

    delet aksebelity

--- android/app/src/main/java/com/nanomindexplorer/gamemappermind/MainActivity.java
+++ android/app/src/main/java/com/nanomindexplorer/gamemappermind/MainActivity.java
@@ -12,35 +12,11 @@ import android.os.Build;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-    private androidx.activity.result.ActivityResultLauncher<Intent> overlayPermissionLauncher;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         registerPlugin(ShizukuPlugin.class);
         registerPlugin(OverlayPlugin.class);
-
-        overlayPermissionLauncher = registerForActivityResult(
-                new androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
-                        android.util.Log.d("GameMapper", "Overlay permission granted! Auto-starting service.");
-                        Intent serviceIntent = new Intent(this, FloatingOverlayService.class);
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                            startForegroundService(serviceIntent);
-                        } else {
-                            startService(serviceIntent);
-                        }
-                    }
-                }
-        );
-
-        // Check Overlay Permission
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
-            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + getPackageName()));
-            overlayPermissionLauncher.launch(intent);
-        }
         
         // Aktifkan Gamepad API & properti WebView
         if (bridge != null && bridge.getWebView() != null) {
